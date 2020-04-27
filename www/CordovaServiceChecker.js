c87dec93fb27d4160a5983d0d7acd31057e87c3e
onurkenis <kenisonur@yandex.com>
2020-04-27 14:18:06 -0500

    feat: implement exported availability functions

--- www/CordovaServiceChecker.js
+++ www/CordovaServiceChecker.js
@@ -1,9 +1,39 @@
 var exec = require("cordova/exec");
 
-exports.isHmsAvailable = function (arg0, success, error) {
-  exec(success, error, "CordovaServiceChecker", "isHmsAvailable", [arg0]);
+exports.isHmsAvailable = async function () {
+  let availability = false;
+
+  await exec(
+    function (rawResult) {
+      availability = JSON.parse(rawResult).result;
+    },
+    isAvailableFail,
+    "CordovaServiceChecker",
+    "isHmsAvailable",
+    []
+  );
+
+  return availability;
 };
 
-exports.isGmsAvailable = function (arg0, success, error) {
-  exec(success, error, "CordovaServiceChecker", "isGmsAvailable", [arg0]);
+exports.isGmsAvailable = async function () {
+  let availability = false;
+
+  await exec(
+    function (rawResult) {
+      availability = JSON.parse(rawResult).result;
+    },
+    isAvailableFail,
+    "CordovaServiceChecker",
+    "isGmsAvailable",
+    []
+  );
+
+  return availability;
 };
+
+function isAvailableFail() {
+  console.log("Availability check: ", "failed");
+
+  return false;
+}
